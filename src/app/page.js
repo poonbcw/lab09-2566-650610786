3c6358c46eb3e71bd41bdb05539c39454ee2a408
Poon Boonchoowit <poon15112@gmail.com>
2023-07-21 23:33:08 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,20 @@ import { useState } from "react";
 export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
+  // const [countAll, setCountAll] = useState(0);
+  // const [countDone, setCountDone] = useState(0);
 
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
+    // setCountAll(countAll + 1);
   };
 
   const deleteTask = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    // setCountAll(countAll - 1);
   };
 
   const toggleDoneTask = (taskId) => {
@@ -41,7 +45,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({tasks.length}) Done ({tasks.filter((x) => x.completed).length})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +64,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Poon Boonchoowit" studentId="650610786" />
     </div>
   );
 }
